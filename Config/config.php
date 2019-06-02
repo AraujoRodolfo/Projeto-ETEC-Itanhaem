b34dfff8f7c93525d915f4427e211ee3db53c295
Jhonatancs <jdc_santos@outlook.com>
2019-06-02 13:50:29 -0300

    adicionando linhas para mostrar erros

--- Config/config.php
+++ Config/config.php
@@ -1,4 +1,9 @@
 <?php
+    // mostrar erros
+    ini_set('display_errors',1);
+    ini_set('display_startup_erros',1);
+    error_reporting(E_ALL);
+    
     //caso nao esteja no root, coloacar o nome da subpasta exemplo: "sub/"
         $subpasta = "";
         
