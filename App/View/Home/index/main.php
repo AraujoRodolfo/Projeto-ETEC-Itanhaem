fc1e784e9794950cc68e4ccba257f26168a4abc8
Jhonatancs <jdc_santos@outlook.com>
2019-06-05 20:53:32 -0300

    testando forma de mostrar os dados non front

--- App/View/Home/index/main.php
+++ App/View/Home/index/main.php
@@ -1,6 +1,6 @@
 <h1>Index</h1>
 	<?php
-		foreach($this->list['ultimas noticias'] as $obj => $value){
+		foreach($this->list['ultimas noticias'] as $key => $obj){
 			echo "<h1>".$obj->getTitulo()."</h1>";
 		}
 	?>
