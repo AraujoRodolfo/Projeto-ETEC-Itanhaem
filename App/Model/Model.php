09f2af2aaae5222d5097ecc3f44bf3426adad344
Jhonatancs <jdc_santos@outlook.com>
2019-06-04 21:08:38 -0300

    testando erro ao decriptar

--- App/Model/Model.php
+++ App/Model/Model.php
@@ -2,16 +2,7 @@
     namespace App\Model;
     //classe generica para as outras herdarem
     class Model {
-    	protected $excessoes;
+    	public $excessoes;
     	
         use \Src\Traits\TraitCrypt;
-
-        public function encryptAll(){
-			foreach(get_object_vars($this) as $key => $value){
-				//se o atributo nao existir no array de excessoes, encripte.
-				if(!in_array($key,$this->excessoes)){
-					$this->$key = $this->encrypt($value,CRYPT_KEY);
-				}
-			}
-		}
     }
--- Src/Traits/TraitCrypt.php
+++ Src/Traits/TraitCrypt.php
@@ -27,6 +27,15 @@
           return $result;
         }
 
+        public function encryptAll(){
+            foreach(get_object_vars($this) as $key => $value){
+                //se o atributo nao existir no array de excessoes, encripte.
+                if(!in_array($key,$this->excessoes)){
+                    $this->$key = $this->encrypt($value,CRYPT_KEY);
+                }
+            }
+        }
+
         public function decryptAll(){
             foreach(get_object_vars($this) as $key => $value){
                 //se o atributo nao existir no array de excessoes, decripte.
