b3c6f65e2689b25cd6f061c24ddad7fb82c030d7
Jhonatancs <jdc_santos@outlook.com>
2019-06-05 21:04:56 -0300

    finalizado teste para mostrar os dados no front

--- App/Controller/ControllerHome.php
+++ App/Controller/ControllerHome.php
@@ -23,6 +23,7 @@
         public function index(){
             //MODEL ===========================================================
             $postDAO = new PostDAO();
+            
             //VIEW ============================================================
             $render = new ClassRender();
             $render->setTitle('Ensino Médio e Técnico gratuito em Itanhaém');
