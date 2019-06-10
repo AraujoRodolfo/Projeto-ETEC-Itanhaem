8709d14fa45ec67b7ad45a07c38ac1ca82ec341f
Jhonatancs <jdc_santos@outlook.com>
2019-06-10 20:14:01 -0300

    adicionados atributos e getters destes atributos no ModelUsuario e corrigido o nome da pasta wirframes]

--- App/Model/ModelUsuario.php
+++ App/Model/ModelUsuario.php
@@ -5,22 +5,44 @@
 
 	class ModelUsuario extends Model {
 		private $id;
+		private $turma;
 		private $nome;
 		private $email;
 		private $senha;
-		private $tipo;
+		private $dtNascimento;
 		private $foto;
+		private $redefinePwUrl;
+		private $dtCriacaoConta;
+		private $dtAtualizacaoConta;
+		private $hrAtendimento;
+		private $social;
+		private $status;
+		private $ip;
+		private $ultimoAcesso;
+		//tabela grupo
+		private $tipo;
+
 
 		private $redefinePwUrl;
 
 		//GETTERS
-		public function getId()		{ 	return $this->id; 		}
-		public function getNome()	{ 	return $this->nome; 	}
-		public function getEmail()	{ 	return $this->email;	}
-		public function getSenha()	{ 	return $this->senha; 	}
-		public function getTipo()	{ 	return $this->tipo; 	}
-		public function getFoto()	{ 	return $this->foto; 	}
+		public function getId() { return $this->id; }
+		public function getTurma() { return $this->turma; }
+		public function getNome() { return $this->nome; }
+		public function getEmail() { return $this->email; }
+		public function getSenha() { return $this->senha; }
+		public function getDtNascimento(){ return $this->dtNascimento; }
+		public function getFoto() { return $this->foto; }
 		public function getRedefinePwUrl(){ return $this->redefinePwUrl; }
+		public function getdtCriacaoConta(){ return $this->dtCriacaoConta; }
+		public function getDtAtualizacaoConta(){ return $this->dtAtualizacaoConta; }
+		public function getHrAtendimento(){ return $this->hrAtendimento; }
+		public function getSocial(){ return $this->social; }
+		public function getStatus(){ return $this->status; }
+		public function getIp(){ return $this->ip; }
+		public function getUltimoAcesso(){ return $this->ultimoAcesso; }
+
+		public function getTipo() { return $this->tipo; }
 
 		//SETTERS
 		public function setId(int $id){
